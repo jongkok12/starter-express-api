23e712d2e5fa235fa679bba8f4ff8f6623e96b83
jongkok12 <118080976+jongkok12@users.noreply.github.com>
2023-04-20 02:36:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ app.get('/', (req, res) => {
       return res.status(500).send('Internal Server Error')
     }
 
+    res.setHeader('Access-Control-Allow-Origin', '*')
     res.json(JSON.parse(data))
   })
 })
@@ -40,6 +41,7 @@ app.get('/', (req, res) => {
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server started on port ${process.env.PORT || 3000}`)
 })
+
 
 
 const axios = require('axios');
