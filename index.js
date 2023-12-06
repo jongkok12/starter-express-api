bca2e03c5fc0427185bb081abd5b8613738693cb
jongkok12 <118080976+jongkok12@users.noreply.github.com>
2023-12-06 10:04:46 +0700

    index.js

--- index.js
+++ index.js
@@ -22,153 +22,150 @@
 // })
 // app.listen(process.env.PORT || 3000)
 /////////////////////////////////////////////////////
-// const express = require('express')
-
-// const app = express()
-
-// app.get('/', (req, res) => {
-//   fs.readFile('data.json', 'utf8', (err, data) => {
-//     if (err) {
-//       console.error(err)
-//       return res.status(500).send('Internal Server Error')
-//     }
-
-//     res.setHeader('Access-Control-Allow-Origin', '*')
-//     res.json(JSON.parse(data))
-//   })
-// })
-
-// app.listen(process.env.PORT || 3000, () => {
-//   console.log(`Server started on port ${process.env.PORT || 3000}`)
-// })
-
-
-
-// const axios = require('axios');
-// const cheerio = require('cheerio');
-// const fs = require('fs');
-
-// axios.get('https://hongkong.pools.wiki/')
-//   .then(response => {
-//     const $ = cheerio.load(response.data);
-//     const tableRows = $('table tr');
-//     const tableData = [];
-
-//     tableRows.each((i, row) => {
-//       const rowData = [];
-//       $(row).find('td').each((j, cell) => {
-//         rowData.push($(cell).text().trim());
-//       });
-//       if (rowData.length > 0) {
-//         tableData.push(rowData);
-//       }
-//     });
-
-//     const headers = tableData.shift();
-//     const jsonData = tableData.map(row => {
-//       const obj = {};
-//       row.forEach((cell, i) => {
-//         obj[headers[i]] = cell;
-//       });
-//       return obj;
-//     });
-
-//     const jsonString = JSON.stringify(jsonData);
-//     fs.writeFile('data.json', jsonString, 'utf8', err => {
-//       if (err) {
-//         console.log(err);
-//       } else {
-//         console.log('Data saved to data.json');
-//       }
-//     });
-//   })
-//   .catch(error => {
-//     console.log(error);
-//   });
-
-
 const express = require('express')
 
 const app = express()
 
+app.get('/', (req, res) => {
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err)
+      return res.status(500).send('Internal Server Error')
+    }
+
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.json(JSON.parse(data))
+  })
+})
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server started on port ${process.env.PORT || 3000}`)
+})
 
 
 
-
-
+const axios = require('axios');
 const cheerio = require('cheerio');
+const fs = require('fs');
 
-
-
-const port = 3000;
-
-
-const url = 'https://w3.angkanet.zone/wp-content/plugins/togelmania/datalive/datalivehk.php';
-
-
-// const url = 'https://w3.angkanet.zone/wp-content/plugins/togelmania/datalive/sd.php';
-
-
-
-
-app.get('/', async (req, res) => {
-
-  try {
-
-    // Menggunakan dynamic import untuk mengimpor 'node-fetch'
-
-    const { default: fetch } = await import('node-fetch');
-
-
-    const response = await fetch(url);
-
-    const html = await response.text();
-
-
-    // Menggunakan cheerio untuk mem-parsing HTML
-
-    const $ = cheerio.load(html);
-
-
-    // Mengekstrak data dari tabel
-
+axios.get('https://hongkong.pools.wiki/')
+  .then(response => {
+    const $ = cheerio.load(response.data);
+    const tableRows = $('table tr');
     const tableData = [];
 
-    $('table tbody tr').each((index, element) => {
-
-      const columns = $(element).find('td');
-
-      const rowData = {
-
-        name: $(columns[0]).text().trim(),
-
-        number: $(columns[1]).text().trim()
-
-      };
-
-      tableData.push(rowData);
-
+    tableRows.each((i, row) => {
+      const rowData = [];
+      $(row).find('td').each((j, cell) => {
+        rowData.push($(cell).text().trim());
+      });
+      if (rowData.length > 0) {
+        tableData.push(rowData);
+      }
     });
 
+    const headers = tableData.shift();
+    const jsonData = tableData.map(row => {
+      const obj = {};
+      row.forEach((cell, i) => {
+        obj[headers[i]] = cell;
+      });
+      return obj;
+    });
 
-    // Kirim data tabel sebagai JSON
+    const jsonString = JSON.stringify(jsonData);
+    fs.writeFile('data.json', jsonString, 'utf8', err => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Data saved to data.json');
+      }
+    });
+  })
+  .catch(error => {
+    console.log(error);
+  });
 
-    res.json(tableData);
+//////////////////////////////////////////////
 
-  } catch (error) {
+// const express = require('express')
 
-    console.error('Error fetching data:', error);
-
-    res.status(500).json({ error: 'Error fetching data' });
-
-  }
-
-});
+// const app = express()
 
 
-app.listen(port, () => {
+// const cheerio = require('cheerio');
 
-  console.log(`Server is running on port ${port}`);
 
-});
+
+// const port = 3000;
+
+
+// const url = 'https://w3.angkanet.zone/wp-content/plugins/togelmania/datalive/datalivehk.php';
+
+
+// // const url = 'https://w3.angkanet.zone/wp-content/plugins/togelmania/datalive/sd.php';
+
+
+
+
+// app.get('/', async (req, res) => {
+
+//   try {
+
+//     // Menggunakan dynamic import untuk mengimpor 'node-fetch'
+
+//     const { default: fetch } = await import('node-fetch');
+
+
+//     const response = await fetch(url);
+
+//     const html = await response.text();
+
+
+//     // Menggunakan cheerio untuk mem-parsing HTML
+
+//     const $ = cheerio.load(html);
+
+
+//     // Mengekstrak data dari tabel
+
+//     const tableData = [];
+
+//     $('table tbody tr').each((index, element) => {
+
+//       const columns = $(element).find('td');
+
+//       const rowData = {
+
+//         name: $(columns[0]).text().trim(),
+
+//         number: $(columns[1]).text().trim()
+
+//       };
+
+//       tableData.push(rowData);
+
+//     });
+
+
+//     // Kirim data tabel sebagai JSON
+
+//     res.json(tableData);
+
+//   } catch (error) {
+
+//     console.error('Error fetching data:', error);
+
+//     res.status(500).json({ error: 'Error fetching data' });
+
+//   }
+
+// });
+
+
+// app.listen(port, () => {
+
+//   console.log(`Server is running on port ${port}`);
+
+// });
 
